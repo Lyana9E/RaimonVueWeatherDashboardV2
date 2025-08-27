53b69e84a93dcb3faec337099a13f491128e30f1
liana <lylaent@gmail.com>
2025-08-27 15:14:02 +0330

    set configuration

--- weather-dashboard/src/main.ts
+++ weather-dashboard/src/main.ts
@@ -1,5 +1,18 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import './style.css'
 import App from './App.vue'
+import router from './router'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/reset.css'
+import './assets/index.css'
 
-createApp(App).mount('#app')
+
+
+const app = createApp(App)
+app.use(createPinia())
+app.use(router)
+app.use(Antd)
+app.mount('#app')
+
+
