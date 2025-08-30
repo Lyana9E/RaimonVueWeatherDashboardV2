d856ae5fe8422d6bc4feab206fc69961db740899
liana <lylaent@gmail.com>
2025-08-30 13:17:29 +0330

    use setup pinia in weatherStore file

--- weather-dashboard/src/stores/weatherStore.ts
+++ weather-dashboard/src/stores/weatherStore.ts
@@ -1,18 +1,32 @@
-import { defineStore } from 'pinia'
-import { useLocalStorage } from '@vueuse/core'
+import {computed} from 'vue'
+import {defineStore} from "pinia";
+import {useLocalStorage} from '@vueuse/core'
 
-export const useWeatherStore = defineStore('weather', {
-    state: () => ({
-        favorites: useLocalStorage<string[]>('favorites', []),
-    }),
-    actions: {
-        addFavorite(city: string) {
-            if (!this.favorites.includes(city)) {
-                this.favorites.push(city)
-            }
-        },
-        removeFavorite(city: string) {
-            this.favorites = this.favorites.filter(c => c !== city)
-        },
-    },
+export const useWeatherStore = defineStore('weather',()=>{
+
+    const favorites = useLocalStorage<string[]>('favorites',[])
+
+
+    const addFavorite =(city:string)=>{
+        if(!favorites.value.includes(city)){
+            favorites.value.push(city)
+
+        }
+
+    }
+
+    const removeFavorite = (city:string)=>{
+        favorites.value = favorites.value.filter(c=>c!==city)
+
+    }
+
+    const favoriteCount = computed(() => favorites.value.length)
+
+    return{
+        favorites,
+        addFavorite,
+        removeFavorite,
+        favoriteCount
+
+    }
 })
