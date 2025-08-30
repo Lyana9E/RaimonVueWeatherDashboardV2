074d99a32760a22b96eafa29bb16b7c4782b832d
liana <lylaent@gmail.com>
2025-08-30 16:52:53 +0330

    add new rout for favorites page

--- weather-dashboard/src/router/index.ts
+++ weather-dashboard/src/router/index.ts
@@ -1,8 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../views/HomePage.vue'
+import FavoritesPage from '../views/FavoritesPage.vue'
+
 
 const routes = [
     { path: '/', name: 'home', component: HomePage },
+    { path: '/favorites', name: 'favorites', component: FavoritesPage },
 ]
 
 const router = createRouter({
